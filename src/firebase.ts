3dc8a45ab52c7104f4c90e85b25d9033b8d3bc97
osasou <osasou0125@gmail.com>
2022-07-23 22:39:19 +0900

    edit firebase setting

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,20 +2,9 @@
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebase-config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "XXXXX",
-  authDomain: "XXXXX.firebaseapp.com",
-  projectId: "XXXXX",
-  storageBucket: "XXXXX.appspot.com",
-  messagingSenderId: "XXXXX",
-  appId: "XXXXX",
-  measurementId: "XXXXX",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
